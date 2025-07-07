a8193812214561ef6ec47bc7a8427eea314fadf9
Harvey Limbo <limboh27@gmail.com>
2025-07-07 13:44:37 -0700

    add dashing enemy

--- scripts/base_enemy.gd
+++ scripts/base_enemy.gd
@@ -1,4 +1,4 @@
-class_name BaseEnemy extends Node2D
+class_name BaseEnemy extends CharacterBody2D
 
 ### dependencies
 @export var event_bus: EventBus = EventBus
@@ -10,11 +10,12 @@ enum AI_State
 	HURT,
 	DEATH,
 	ATTACK,
+	PREP_ATTACK,
 }
 
 @export var starting_hp: float = 1000.0
 @export var hp: float
-@export var follow_speed: float = 100.0
+@export var follow_speed: float = 100.0 # pixels per second
 @export var follow_distance: float = 32.0
 @export var ai_state: AI_State = AI_State.IDLE
 var prev_ai_state: AI_State = ai_state
@@ -29,6 +30,7 @@ var prev_ai_state: AI_State = ai_state
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var hit_area: Area2D = $HitArea
 @onready var damage_text_root: Node2D = $DamageTextRoot
+@onready var ai_state_label: Label = $DebugTextRoot/AIStateLabel
 @onready var hurt_timer: Timer = $hurt_timer
 @onready var death_timer: Timer = $death_timer
 
@@ -49,7 +51,7 @@ func clear_shader():
 	self.material = null
 
 func can_attack() -> bool:
-	return ai_state != AI_State.ATTACK and (is_instance_valid(target) and position.distance_to(target.position) < follow_distance)
+	return ![AI_State.ATTACK, AI_State.PREP_ATTACK].has(ai_state) and (is_instance_valid(target) and position.distance_to(target.position) < follow_distance)
 
 func _ready():
 	detection_area.body_entered.connect(on_body_entered)
@@ -74,16 +76,11 @@ func on_death_completed():
 	queue_free()
 
 func _physics_process(delta_time: float):
-	if ai_state == AI_State.FOLLOW:
-		if not is_instance_valid(target):
-			ai_state = AI_State.IDLE
-		else:
-			_aim_dir = (target.position - position).normalized()
-			follow(delta_time)
+	self.handle_states()
 	
 	aim_line.points = [
-		Vector2.ZERO,                          # start at player origin
-		_aim_dir * aim_line_length                           # outwards along aim
+		Vector2.ZERO,                         
+		_aim_dir * aim_line_length
 	]
 	
 	sprite_2d.flip_h = _aim_dir.x > 0
@@ -91,15 +88,43 @@ func _physics_process(delta_time: float):
 	if can_attack():
 		start_attack()
 	
+	# apply all velocities/accelerations
+	self.move_and_slide()
+	
 func _process(delta: float):
+	update_ai_state_label()
 	if ai_state == AI_State.DEATH:
 		var shader_mat: ShaderMaterial = (material as ShaderMaterial)
 		if is_instance_valid(shader_mat):
 			var t: float = 1.0 - curr_death_time / death_timer.wait_time
 			shader_mat.set_shader_parameter("alpha", t)
 			curr_death_time += delta
+			
+func update_ai_state_label():
+	match ai_state:
+		AI_State.IDLE:
+			ai_state_label.text = "IDLE"
+		AI_State.FOLLOW:
+			ai_state_label.text = "FOLLOW"
+		AI_State.HURT:
+			ai_state_label.text = "HURT"
+		AI_State.DEATH:
+			ai_state_label.text = "DEATH"
+		AI_State.ATTACK:
+			ai_state_label.text = "ATTACK"
+		AI_State.PREP_ATTACK:
+			ai_state_label.text = "PREP_ATTACK"
 
 #region overridable functions
+func handle_states():
+	if ai_state == AI_State.FOLLOW:
+		if not is_instance_valid(target):
+			ai_state = AI_State.IDLE
+		else:
+			_aim_dir = (target.position - position).normalized()
+			follow()
+
+
 func on_enter_impact_area(explosion: BaseExplosion, actor: Node):
 	# if same actor that received impact, handle the signal, otherwise don't handle it
 	if actor != self:
@@ -115,10 +140,12 @@ func on_enter_impact_area(explosion: BaseExplosion, actor: Node):
 	
 	if hp > 0:
 		ai_state = AI_State.HURT
+		self.velocity = Vector2(0.0, 0.0)
 		set_shader(hurt_shader)
 		hurt_timer.start()
 	else:
 		ai_state = AI_State.DEATH
+		self.velocity = Vector2(0.0, 0.0)
 		set_shader(death_shader)
 		hit_area.queue_free()
 		detection_area.queue_free()
@@ -133,12 +160,12 @@ func on_exit_impact_area(explosion: BaseExplosion, actor: Node):
 	if actor != self:
 		return
 	
-func follow(delta_time: float):
+func follow():
 	var diff: Vector2 = target.position - position
 	var distance: float = Vector2.ZERO.distance_to(diff)
 	var dir: Vector2 = diff.normalized()
-	curr_move_velocity = Vector2.ZERO if distance < follow_distance else dir * follow_speed * delta_time
-	position = position + curr_move_velocity
+	curr_move_velocity = Vector2.ZERO if distance < follow_distance else dir * follow_speed
+	self.velocity = curr_move_velocity
 
 func start_attack():
 	prev_ai_state = ai_state
---
+++ scripts/dashing_enemy.gd
@@ -0,0 +1,77 @@
+class_name DashingEnemy extends BaseEnemy
+
+@onready var dash_attack_timer: Timer = $dash_attack_timer
+@onready var cooldown_timer: Timer = $cooldown_timer
+@onready var attack_area: Area2D = $AttackArea
+@onready var dash_line: Line2D = $DashLine
+
+@export var dash_attack_distance: float = 90.0 # units
+@export var max_dash_velocity: float = 1000.0
+@export var dash_acceleration: float = 100.0
+
+var start_dash_position: Vector2 = Vector2.ZERO
+var dash_dir: Vector2 = Vector2.ZERO
+var can_dash: bool = true
+
+func _ready():
+	super()
+	dash_attack_timer.timeout.connect(start_dash_attack)
+	cooldown_timer.timeout.connect(on_refresh_cooldown)
+	attack_area.body_entered.connect(on_dash_attack_connected)
+	
+func on_dash_attack_connected(other: Node2D):
+	event_bus.on_start_attack.emit(self, other)
+	complete_dash()
+	
+func start_dash_attack():
+	if ai_state == AI_State.ATTACK:
+		return
+		
+	ai_state = AI_State.ATTACK
+
+func on_refresh_cooldown():
+	can_dash = true
+
+func complete_dash():
+	self.velocity = Vector2(0.0, 0.0)
+	dash_line.visible = false
+	ai_state = AI_State.FOLLOW
+	prev_ai_state = AI_State.IDLE
+	cooldown_timer.start()
+
+#region overridable functions
+func handle_states():
+	super()
+	
+	if ai_state == AI_State.ATTACK:
+		var velocity_a: Vector2 = velocity + dash_dir * dash_acceleration
+		var velocity_b: Vector2 = dash_dir * max_dash_velocity
+		if velocity_a.length_squared() > velocity_b.length_squared():
+			self.velocity = velocity_b
+		else:
+			self.velocity = velocity_a
+		
+		var curr_dash_distance: float = start_dash_position.distance_to(position)
+		# if reached or exceeded dash attack distance switch back to follow state
+		if curr_dash_distance >= dash_attack_distance:
+			complete_dash()
+		
+func start_attack():
+	# if dash attack on cooldown, don't dash
+	if not can_dash:
+		return
+	
+	prev_ai_state = ai_state
+	ai_state = AI_State.PREP_ATTACK
+	can_dash = false
+	
+	# draw preview of where dash will move towards using Line2D node
+	dash_dir = (target.position - position).normalized()
+	var dash_preview_vector: Vector2 = dash_dir * dash_attack_distance
+	dash_line.points = [Vector2.ZERO, dash_preview_vector]
+	dash_line.visible = true
+	
+	velocity = Vector2.ZERO
+	start_dash_position = position
+	dash_attack_timer.start()
+#endregion
