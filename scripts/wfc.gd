5d9c2f02c924b3eee8b91cd0e319ff75df8b377e
Harvey Limbo <limboh27@gmail.com>
2025-07-18 17:25:55 -0700

    implement init() function

--- scripts/wfc.gd
+++ scripts/wfc.gd
@@ -3,10 +3,14 @@
 
 extends Node
 
+# goal: have an NxM grid of characters generate a map of characters outputted as console log
 # O - Ocean
 # M - Mountain
 # F - Forest
 # S - Sand
+const TILES: Array[String] = ["O", "M", "F", "S"]
+
+@onready var button: Button = $my_button
 
 enum Compass { N = 0, S = 1, W = 2, E = 3 }
 
@@ -50,53 +54,89 @@ class PossibleTiles:
 		tiles = []
 		tiles.append_array(_tiles)
 
-# represents the possible tile selections the algorithm can pick from
-# gets collapsed down to 1 tile per cell once algorithm finishes
-# first index - row position (y position)
-# second index - col position (x position)
-# value - string representing tile for now (will swap out with actual tiles later on)
-var wave_state: Array[Array]
-var width: int
-var height: int
-var tiles_count: int
-
-# end of array represents the latest banned tile
-# beginning of array represents oldest banned tile
-var banned_tiles_stack: Array[BannedTile]
-# keeps track of where the latest banned tile is
-var stack_size: int
-
 # a lookup table to determine which neighboring tiles are valid to pick 
 # when a tile at a given position is chosen
 # key - cell index on map
 # value - list of possible tiles to pick from where each entry represents a neighboring tile
 var possible_neighboring_tiles_table: Dictionary[Vector2i, PossibleNeighborTiles]
 
+@export var width: int = 10
+@export var height: int = 10
+@export var tiles_count: int = 4
+
 # this represents how many tile options are left for a given cell
+# gets collapsed down to 1 tile per cell once algorithm finishes
 # 2D array of PossibleTiles
 # first index represents row (y)
 # second index represents col (x)
 var possible_tile_choices: Array[Array]
+# represents the final tile map output to console log
+# 2d Array of Strings
+var wave_output: Array[Array]
+
+# end of array represents the latest banned tile
+# beginning of array represents oldest banned tile
+var banned_tiles_stack: Array[BannedTile] = []
+# keeps track of where the latest banned tile is
+var stack_size: int
+
 
 ### Weights - used to decide which tile to pick for a given cell
-### these are all 2d arrays containing floats
-var weights: Array[Array]
-var log_weights: Array[Array]
+### these are all arrays containing floats
+### index is tile index
+var weights: Array[float]
+var log_weights: Array[float]
 # represents the current pick distribution rate when selecting a tile for a given cell
-var distribution: Array[Array]
+var distribution: Array[float]
 
-var sum_of_weights: float
-var sum_of_log_weights: float
-var starting_entropy: float
+var sum_of_weights: float = 0.0
+var sum_of_log_weights: float = 0.0
+var starting_entropy: float = 0.0
 
 ### these are all 2d arrays containing floats
 var sums_of_weights: Array[Array]
 var sums_of_log_weights: Array[Array]
-
+var entropies: Array[Array]
 
 
 func init():
-	pass
+	wave_output.resize(height)
+	possible_tile_choices.resize(height)
+	sums_of_weights.resize(height)
+	sums_of_log_weights.resize(height)
+	entropies.resize(height)
+	
+	for row in range(height):
+		wave_output[row].resize(width)
+		possible_tile_choices[row].resize(width)
+		sums_of_weights[row].resize(width)
+		sums_of_log_weights[row].resize(width)
+		entropies[row].resize(width)
+		for col in range(width):
+			wave_output[row][col] ="."
+			possible_tile_choices[row][col] = PossibleTiles.new(TILES)
+			sums_of_weights[row][col] = 0.0
+			sums_of_log_weights[row][col] = 0.0
+			entropies[row][col] = 0.0
+	
+	weights.resize(tiles_count)
+	log_weights.resize(tiles_count)
+	distribution.resize(tiles_count)
+	
+	# compute weights using Shannon Entropy
+	# Source: https://github.com/mxgmn/WaveFunctionCollapse/blob/master/Model.cs
+	# starts at line 55 of source code link
+	for t in range(tiles_count):
+		weights[t] = 2.0 # equal weight
+		log_weights[t] = weights[t] * log(weights[t])
+		sum_of_weights += weights[t]
+		sum_of_log_weights += log_weights[t]
+	
+	assert(sum_of_weights > 0)
+	starting_entropy = log(sum_of_weights) - (sum_of_log_weights / sum_of_weights)
+	
+	banned_tiles_stack.resize(width * height)
+	stack_size = 0
 
 func clear():
 	pass
@@ -135,3 +175,7 @@ func run() -> bool:
 			break
 	
 	return is_puzzle_solved
+
+
+func _ready():
+	button.pressed.connect(run)
