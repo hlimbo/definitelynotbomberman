2eb36de6d8932f5f7d8aeb7fae421e4755c3b48b
Harvey Limbo <limboh27@gmail.com>
2025-07-30 19:18:57 -0700

    implement code for random spawn picker and weight table scripts

---
+++ scripts/random_spawn_picker.gd
@@ -0,0 +1,62 @@
+class_name RandomSpawnPicker extends Node
+
+const INVALID_POSITION = Vector2i(-1, -1)
+
+@export var tile_map_layer: TileMapLayer
+# position of the tile map layer node relative to the world position
+var root_position: Vector2
+var tile_map_layer_scale: Vector2
+# x - width in pixels
+# y - height in pixels
+var tile_dimensions: Vector2i
+var tile_map_width: int
+var tile_map_height: int
+
+# represents what positions are valid to spawn packed scenes in
+# 2d array of bools
+# true - can spawn a packed scene in the given [y][x] tile coordinate position
+# false - cannot spawn a packed scene in the given [y][x] tile coordinate position
+var available_positions: Array[Array]
+
+func _ready():
+	tile_map_layer_scale = tile_map_layer.scale
+	tile_dimensions = tile_map_layer.tile_set.tile_size
+
+	# returns enclosing rect space of the tile map layer
+	var rect_area: Rect2i = tile_map_layer.get_used_rect()
+	root_position = tile_map_layer.position + Vector2(rect_area.position)
+	tile_map_width = rect_area.size.x
+	tile_map_height = rect_area.size.y
+
+	reset()
+
+func pick_random_spawn_location() -> Vector2i:
+	# collect all randomly available positions
+	var valid_positions: Array[Vector2i] = []
+	for y in range(tile_map_height):
+		for x in range(tile_map_width):
+			var is_available: bool = available_positions[y][x]
+			if is_available:
+				valid_positions.append(Vector2i(x,y))
+	
+	var random_index: int = -1 if len(valid_positions) == 0 else randi_range(0, len(valid_positions) - 1)
+	if random_index == -1:
+		return INVALID_POSITION
+	
+	var random_position: Vector2i = valid_positions[random_index]
+	# mark is unavailable position to spawn in
+	available_positions[random_position.y][random_position.x] = false	
+	return random_position
+
+# sets all cell positions as valid positions to randomly place a node on
+func reset():
+	available_positions = []
+	for row in range(tile_map_height):
+		available_positions.append([])
+		for col in range(tile_map_width):
+			available_positions[row].append(true)
+	
+func convert_tile_coords_to_world_coords(tile_coord: Vector2i) -> Vector2:
+	var x_offset: float = tile_coord.x * float(tile_dimensions.x) * tile_map_layer_scale.x
+	var y_offset: float = tile_coord.y * float(tile_dimensions.y) * tile_map_layer_scale.y
+	return root_position + 0.5 * Vector2(x_offset, y_offset)
---
+++ scripts/weight_table.gd
@@ -0,0 +1,73 @@
+class_name WeightTable extends Resource
+
+# key represents what can be spawned in a scene
+# value represents the weight of the PackedScene. Used to determine the likelihood of spawning the PackedScene.
+# higher weight values means higher likelihood of spawning. lower weight values means lower likelihood of spawning.
+@export var original_pool: Dictionary[PackedScene, float] = {}
+# represents what can be randomly spawned
+var active_pool: Dictionary[PackedScene, float] = {}
+# represents what isn't available to be randomly spawned
+var inactive_pool: Dictionary[PackedScene, bool] = {}
+
+# used to determine if only unique picks are required
+@export var is_unique_only: bool = false
+
+var sum_weights: float = 0.0
+
+func construct():
+	for packed_scene in original_pool:
+		active_pool[packed_scene] = original_pool[packed_scene]
+		inactive_pool[packed_scene] = false
+		
+	sum_weights = compute_sum_weights()
+
+func compute_sum_weights() -> float:
+	var weights_sum: float = 0.0
+	for packed_scene in active_pool:
+		var weight: float = active_pool[packed_scene]
+		weights_sum += weight
+
+	return weights_sum
+	
+func pick_random_spawnable_packed_scene() -> PackedScene:
+	assert(len(active_pool) > 0)
+	var packed_scene: PackedScene = active_pool.values()[len(active_pool) - 1] as PackedScene
+	
+	var random_val: float = randf() * sum_weights
+	var total_weight: float = 0.0
+	# using linear scan algorithm... there exists alternatives like binary search or hopscotch
+	# but for a game with a few choices linear scan is easiest to implement and least error prone
+	for scene in active_pool:
+		var weight: float = active_pool[scene]
+		total_weight += weight
+		if total_weight > random_val:
+			packed_scene = scene
+			break
+	
+	# remove from active pool to ensure unique scenes are picked
+	if is_unique_only:
+		remove_packed_scene_from_active_pool(packed_scene)
+		
+	return packed_scene
+
+func remove_packed_scene_from_active_pool(scene: PackedScene) -> bool:
+	var is_erased: bool = active_pool.erase(scene)
+	if is_erased:
+		# mark scene as inactive
+		inactive_pool[scene] = true
+		var weight: float = original_pool[scene]
+		sum_weights -= weight
+	
+	return is_erased
+	
+func add_packed_scene_to_active_pool(scene: PackedScene) -> bool:
+	assert(scene in original_pool)
+	if scene in inactive_pool and inactive_pool[scene]:
+		# mark scene as active
+		inactive_pool[scene] = false
+		active_pool[scene] = original_pool[scene]
+		var weight: float = original_pool[scene]
+		sum_weights += weight
+		return true
+	
+	return false
