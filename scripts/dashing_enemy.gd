3e2a6f4df781c3039636d8f60678c78f55c17f40
Harvey Limbo <limboh27@gmail.com>
2025-08-18 13:50:48 -0700

    add timer to dashing enemy attack to have them switch back to previous state -- band aid fix

--- scripts/dashing_enemy.gd
+++ scripts/dashing_enemy.gd
@@ -7,21 +7,26 @@ class_name DashingEnemy extends BaseEnemy
 @onready var dash_audio_player: AudioStreamPlayer2D = $dash_audio_player
 
 @export var dash_attack_distance: float = 90.0 # units
+# measured in milliseconds
+@export var dash_attack_duration: int = 3000
 @export var max_dash_velocity: float = 1000.0
 @export var dash_acceleration: float = 100.0
 
 var start_dash_position: Vector2 = Vector2.ZERO
 var dash_dir: Vector2 = Vector2.ZERO
 var can_dash: bool = true
+# measured in milliseconds
+var start_dash_duration: int = 0
+var curr_dash_duration: int = 0
 
 func _ready():
 	super()
 	dash_attack_timer.timeout.connect(start_dash_attack)
 	cooldown_timer.timeout.connect(on_refresh_cooldown)
 	attack_area.body_entered.connect(on_dash_attack_connected)
-
+	
 func on_dash_attack_connected(other: Node2D):
-	if other.name != "Player":
+	if other is not Player:
 		return
 	
 	event_bus.on_start_attack.emit(self, other)
@@ -69,8 +74,10 @@ func handle_states():
 			self.velocity = velocity_a
 		
 		var curr_dash_distance: float = start_dash_position.distance_to(position)
+		
+		curr_dash_duration = Time.get_ticks_msec() - start_dash_duration
 		# if reached or exceeded dash attack distance switch back to follow state
-		if curr_dash_distance >= dash_attack_distance:
+		if curr_dash_distance >= dash_attack_distance or curr_dash_duration >= dash_attack_duration:
 			complete_dash()
 
 func start_attack():
@@ -80,6 +87,8 @@ func start_attack():
 	
 	ai_state = AI_State.PREP_ATTACK
 	can_dash = false
+	curr_dash_duration = 0
+	start_dash_duration = Time.get_ticks_msec()
 	
 	# draw preview of where dash will move towards using Line2D node
 	dash_dir = (target.position - position).normalized()
